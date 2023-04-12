60c56d33ee816470a45ab04ed9682f9dcb061ba9
Elena L <57563030+eilmadc@users.noreply.github.com>
2023-04-12 15:17:19 +0200

    Add files via upload

---
+++ demo-gradle/src/main/java/com/example/demo/DemoGradleApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGradleApplication.class, args);
+	}
+	
+
+	 @GetMapping("/")
+	    public String welcome(@RequestParam(value = "name", defaultValue = "World") String name) {
+	      return String.format("Bienvenido a Spring %s!", name);
+	    }
+
+	 
+	 @GetMapping("/hello")
+	    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	      return String.format("Hello %s!", name);
+	    }
+	 
+	 @GetMapping("/saluda")
+	    public String saluda(@RequestParam(value = "name", defaultValue = "World") String name) {
+	      return String.format("Hello %s!", name);
+	    }
+
+}
+
